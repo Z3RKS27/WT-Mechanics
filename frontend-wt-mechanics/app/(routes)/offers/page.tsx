2dc6e3a6858b59c60365f6ad08984b793a0128d4
Z3RKS27 <145464913+Z3RKS27@users.noreply.github.com>
2025-06-22 22:41:57 -0600

    nuevas paginas

--- frontend-wt-mechanics/app/(routes)/offers/page.tsx
+++ frontend-wt-mechanics/app/(routes)/offers/page.tsx
@@ -3,7 +3,7 @@
 export default function Page() {
     return (
         <div className="max-w-4xl py-4 mx-auto sm:py-32 sm:px-24">
-            <h1 className="sm:text-2xl font-bold">Tienda</h1>
+            <h1 className="sm:text-2xl font-bold">Ofertas</h1>
             <p>Esta página está en construcción.</p>
             <p>Pronto podrás ver los productos disponibles.</p>
         </div>
